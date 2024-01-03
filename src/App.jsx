486edd3586752161ae51f3a2c5890d1261a51713
Kiran Gautam <ukiras@gmail.com>
2024-01-03 14:55:42 +1100

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <h1>This is new project... Coming soon</h1>
+      <Routes>
+        <Route path='/' element={<h1>This is new project... Coming soon</h1>}></Route>
+      </Routes>
+
     </>
   )
 }
