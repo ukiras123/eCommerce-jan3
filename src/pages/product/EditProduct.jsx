453c3b08e36efc856011f505eb642dc7481bf0ff
Kiran Gautam <ukiras@gmail.com>
2024-01-11 11:57:23 +1100

    up

--- src/pages/product/EditProduct.jsx
+++ src/pages/product/EditProduct.jsx
@@ -1,21 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import AdminLayout from '../../components/layout/AdminLayout'
 import { Link, useNavigate, useParams } from 'react-router-dom'
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, ProgressBar } from 'react-bootstrap';
 import CustomInput from '../../components/customInput/CustomInput';
 import slugify from 'slugify';
 import { addOrUpdateProductAction } from '../../redux/product/productAction';
 import { useDispatch, useSelector } from 'react-redux';
+import { IoMdClose } from "react-icons/io";
+import { toast } from 'react-toastify';
+import { handleFileUpload } from '../../utils';
 
 function EditProduct() {
     const { slug } = useParams();
     const { productList } = useSelector(state => state.product);
     const { categoryList } = useSelector(state => state.category)
-
+    const imageElemRef = useRef(null);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [formData, setFormData] = useState({
     })
+    const [files, setFiles] = useState([]);
+    const [progress, setProgress] = useState(0)
+
 
     useEffect(() => {
         const selectedProduct = productList.find(p => p.slug === slug);
@@ -76,11 +82,35 @@ function EditProduct() {
             [name]: value
         })
     }
-    const handleOnSubmit = (e) => {
+    const handleOnSubmit = async (e) => {
         e.preventDefault();
-        const finalData = { ...formData, slug }
+        // If user has uploaded new photos, upload it to Firebase, grab the url then modify the finalData
+        let newFileUrl = [];
+        if (files.length > 0) {
+            const filePromise = files.map(file => handleFileUpload(file, setProgress))
+            newFileUrl = await Promise.all(filePromise);
+        }
+        const finalListOfImages = [...formData?.images, ...newFileUrl];
+        const finalData = { ...formData, slug, images: finalListOfImages }
         dispatch(addOrUpdateProductAction(finalData))
-
+        imageElemRef.current.value = '';
+    }
+    const handleThumbnailChange = (url) => {
+        setFormData({ ...formData, thumbnail: url })
+    }
+    const handleImageAttachment = (e) => {
+        const { files } = e.target;
+        setFiles([...files])
+    }
+    const handleImageRemove = (url) => {
+        if (url === formData.thumbnail) {
+            return toast.error("Thumbnail can't be removed")
+        }
+        const remainingImages = formData?.images?.filter(img => img !== url);
+        setFormData({
+            ...formData,
+            images: remainingImages
+        })
     }
     return (
         <AdminLayout title="Edit Product">
@@ -101,9 +131,19 @@ function EditProduct() {
                     {inputFields.map(field => {
                         return <CustomInput key={field.label} {...field} onChange={handleOnChange} />
                     })}
+                    <div className='d-flex gap-2 p-1 rounded flex-wrap'>
+                        {formData?.images?.map(img => {
+                            return <div key={img}>
+                                <IoMdClose onClick={() => handleImageRemove(img)} />
+                                <div onClick={() => handleThumbnailChange(img)} className={img === formData?.thumbnail ? "bg-info p-1" : ''}> <img src={img} width={"150px"} /></div>
 
+                            </div>
+                        })}
+                    </div>
                     <Form.Group className="mb-3">
-                        <Form.Control multiple type="file" accept='image/png, image/jpeg' />
+                        <Form.Control ref={imageElemRef} onChange={handleImageAttachment} multiple type="file" accept='image/png, image/jpeg' />
+                        {progress !== 100 ? <ProgressBar animated now={progress} label={`${progress.toFixed(2)}%`} /> : <ProgressBar variant='success' now={100} visuallyHidden />}
+
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Update
@@ -112,8 +152,8 @@ function EditProduct() {
                 </Form>
 
 
-            </div>
-        </AdminLayout>
+            </div >
+        </AdminLayout >
     )
 }
 
